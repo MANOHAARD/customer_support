2328a57bf654f6698ec5e1c88f4e31f2f13e0656
manohaard <manohaar.d@gmail.com>
2021-08-15 15:13:12 +0530

    second commit

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -32,7 +32,7 @@ def admin_only(view_func):
     def wrapper_func(request, *args, **kwargs):
         group = None
         if request.user.groups.exists():
-            group = request.user.groups.all()[0].name
+            group = request.users.groups.all()[0].name
         print(group)
         if group == 'customer':
             return redirect("accounts:user_page")
